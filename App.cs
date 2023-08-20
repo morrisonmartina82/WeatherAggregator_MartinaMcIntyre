26ace99b4c421c7da50de1e4569ac11f353580b2
morri <morri@LAPTOP-6C7TUT4V>
2023-08-20 15:56:12 +0100

    Add project files.

---
+++ App.cs
@@ -0,0 +1,35 @@
+﻿using WeatherAggregatorBLL.Interfaces;
+
+namespace WeatherAggregator_MartinaMcIntyre
+{
+    public class App
+    {
+        private readonly IDataReaderFactory _drf;
+
+        public App(IDataReaderFactory drf)
+        {
+            _drf = drf;
+        }
+
+        public void Run(string[] args)
+        {
+            string fileName = "weatherData.json";
+            string jsonString = File.ReadAllText(fileName);
+
+            string xmlfileName = "weatherData.xml";
+            string xmlString = File.ReadAllText(xmlfileName);
+
+            if (!String.IsNullOrEmpty(jsonString))
+            {
+                IWeatherPersistence fac = _drf.Create("jsonData");
+                bool message = fac.persistWeatherData(jsonString, "Google Weather", "JSON File");
+            }
+
+            if (!String.IsNullOrEmpty(xmlString))
+            {
+                IWeatherPersistence second = _drf.Create("xmlData");
+                bool message = second.persistWeatherData(xmlString, "MSN Weather", "XML File");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WeatherAggregator_MartinaMcIntyre;
+using WeatherAggregatorBLL.Factories;
+using WeatherAggregatorDAL;
+using WeatherAggregatorDAL.DBModel;
+using WeatherAggregatorDAL.Interface;
+
+using IHost host = CreateHostBuilder(args).Build();
+using var scope = host.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    services.GetRequiredService<App>().Run(args);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+            IConfiguration configuration = hostContext.Configuration;
+            services.AddSingleton<App>();
+            services.AddSingleton<IPersistData, PersistData>();
+            services.AddWeatherDataReader();
+            services.AddDbContext<WeatherDbContext>(options => options.UseSqlServer(
+                           configuration.GetConnectionString("WeatherConnection")));
+        });
+}
